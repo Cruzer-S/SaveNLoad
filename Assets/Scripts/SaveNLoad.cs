a246625f74bdbde662e00b7ac909708ebd74f99f
Cruzer-S <yyyynoom@gmail.com>
2023-06-07 12:14:20 +0900

    implement saving dictionary type

--- Assets/Scripts/SaveNLoad.cs
+++ Assets/Scripts/SaveNLoad.cs
@@ -3,7 +3,9 @@ using System.IO;
 using System.Reflection;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
+
 
 using UnityEngine;
 
@@ -11,16 +13,28 @@ using UnityEngine;
 public class CanSaveNLoad : Attribute
 {
     public string PathName { get; private set; }
+    public BindingFlags BindingFlags { get; private set; }
 
-    public CanSaveNLoad(string PathName) => this.PathName = PathName;
+    public CanSaveNLoad(string pathName, BindingFlags bindingFlags = BindingFlags.Default)
+    {
+        PathName = pathName;
+        BindingFlags = bindingFlags;
+    }
 }
 
 public static partial class SaveNLoad
 {
     private static string ListFormat = "{0}#{1}";
     private static string ListCount = ":Count";
+    private static string DictCount = ":DictCount";
+    private static string DictKeyFormat = "{0}${1}";
+    private static string DictValueFormat = "{0}@{1}";
 
-    public static bool Save<T>(T data, string name) => Save(data.GetType(), data, name);
+    public static bool Save<T>(T data, string name)
+    {
+        Delete(data.GetType(), name);
+        return Save(data.GetType(), data, name);
+    }
 
     public static T Load<T>(string name) => (T)Load(typeof(T), name);
 
@@ -38,7 +52,7 @@ public static partial class SaveNLoad
         if (attribute == null)
             return false;
 
-        fields = type.GetFields();
+        fields = type.GetFields(attribute.BindingFlags);
         foreach (FieldInfo field in fields)
         {
             string key;
@@ -52,7 +66,9 @@ public static partial class SaveNLoad
             if (field.FieldType.GetCustomAttribute(typeof(CanSaveNLoad)) != null)
                 Save(field.FieldType, value, string.Empty, Path.Combine(attribute.PathName, name));
             else if (field.FieldType.IsGenericType && (field.FieldType.GetGenericTypeDefinition() == typeof(List<>)))
-                SaveListType(field.FieldType, key, value, ListFormat);
+                SaveListType(field.FieldType, key, value);
+            else if (field.FieldType.IsGenericType && (field.FieldType.GetGenericTypeDefinition() == typeof(Dictionary<,>)))
+                SaveDictionaryType(field.FieldType, key, value);
             else
                 SaveBasicType(field.FieldType, key, value);
         }
@@ -74,7 +90,7 @@ public static partial class SaveNLoad
             PlayerPrefs.SetInt(key, (char)value);
     }
 
-    private static void SaveListType(Type type, string key, object values, string format)
+    private static void SaveListType(Type type, string key, object values)
     {
         Type elementType = type.GetGenericArguments().Single();
         IList list = values as IList;
@@ -83,12 +99,42 @@ public static partial class SaveNLoad
 
         for (int i = 0; i < list.Count; i++)
         {
-            string formattedKey = string.Format(format, key, i);
+            string formattedKey = string.Format(ListFormat, key, i);
 
             if (elementType.GetCustomAttribute(typeof(CanSaveNLoad)) != null)
                 Save(elementType, list[i], string.Empty, formattedKey);
             else
                 SaveBasicType(elementType, formattedKey, list[i]);
+        }
+    }
+
+    private static void SaveDictionaryType(Type type, string key, object values)
+    {
+        IDictionary dictionary = values as IDictionary;
+        Type keyType = type.GetGenericArguments()[0];
+        Type valueType = type.GetGenericArguments()[1];
+
+        PlayerPrefs.SetInt(key + DictCount, dictionary.Count);
+
+        int index = 0;
+        foreach (object dictKey in dictionary.Keys)
+        {
+            string formattedKey = string.Format(DictKeyFormat, key, index);
+            string formattedValue = string.Format(DictValueFormat, key, index);
+
+            object dictValue = dictionary[dictKey];
+
+            if (keyType.GetCustomAttribute(typeof(CanSaveNLoad)) != null)
+                Save(keyType, dictKey, string.Empty, formattedKey);
+            else
+                SaveBasicType(keyType, formattedKey, dictKey);
+
+            if (valueType.GetCustomAttribute(typeof(CanSaveNLoad)) != null)
+                Save(valueType, dictValue, string.Empty, formattedValue);
+            else
+                SaveBasicType(keyType, formattedValue, dictValue);
+
+            index++;
         }
     }
 }
@@ -97,14 +143,16 @@ public static partial class SaveNLoad
 {
     private static object Load(Type type, string name, string basename = null)
     {
-        FieldInfo[] fields = type.GetFields();
+        FieldInfo[] fields;
         CanSaveNLoad attribute;
 
         attribute = type.GetCustomAttribute<CanSaveNLoad>();
         if (attribute == null)
             return null;
 
-        object @object = Activator.CreateInstance(type);
+        fields = type.GetFields(attribute.BindingFlags);
+
+        object @object = Activator.CreateInstance(type, true);
         if (@object == null)
             return null;
 
@@ -121,7 +169,9 @@ public static partial class SaveNLoad
             if (field.FieldType.GetCustomAttribute(typeof(CanSaveNLoad)) != null)
                 value = Load(field.FieldType, string.Empty, Path.Combine(attribute.PathName, name));
             else if (field.FieldType.IsGenericType && (field.FieldType.GetGenericTypeDefinition() == typeof(List<>)))
-                value = LoadListType(field.FieldType, key, ListFormat);
+                value = LoadListType(field.FieldType, key);
+            else if (field.FieldType.IsGenericType && (field.FieldType.GetGenericTypeDefinition() == typeof(Dictionary<,>)))
+                value = LoadDictionaryType(field.FieldType, key);
             else
                 value = LoadBasicType(field.FieldType, key);
 
@@ -155,7 +205,7 @@ public static partial class SaveNLoad
         return value;
     }
 
-    private static object LoadListType(Type type, string key, string format)
+    private static object LoadListType(Type type, string key)
     {
         Type elementType = type.GetGenericArguments().Single();
         IList list = (IList)Activator.CreateInstance(type);
@@ -171,9 +221,9 @@ public static partial class SaveNLoad
             object data = null;
 
             if (elementType.GetCustomAttribute(typeof(CanSaveNLoad)) != null)
-                data = Load(elementType, string.Empty, string.Format(format, key, i));
-            else if (PlayerPrefs.HasKey(string.Format(format, key, i)))
-                data = LoadBasicType(elementType, string.Format(format, key, i));
+                data = Load(elementType, string.Empty, string.Format(ListFormat, key, i));
+            else if (PlayerPrefs.HasKey(string.Format(ListFormat, key, i)))
+                data = LoadBasicType(elementType, string.Format(ListFormat, key, i));
 
             if (data == null)
                 return null;
@@ -182,6 +232,44 @@ public static partial class SaveNLoad
         }
 
         return list;
+    }
+
+    private static object LoadDictionaryType(Type type, string key)
+    {
+        IDictionary dictionary = (IDictionary) Activator.CreateInstance(type);
+        Type keyType = type.GetGenericArguments()[0];
+        Type valueType = type.GetGenericArguments()[1];
+
+        string countKey = key + DictCount;
+        int count;
+
+        if (!PlayerPrefs.HasKey(countKey))
+            return null;
+
+        count = PlayerPrefs.GetInt(countKey);
+
+        for (int i = 0; i < count; i++)
+        {
+            object dictKey = null;
+            object dictValue = null;
+
+            if (keyType.GetCustomAttribute(typeof(CanSaveNLoad)) != null)
+                dictKey = Load(keyType, string.Empty, string.Format(DictKeyFormat, key, i));
+            else if (PlayerPrefs.HasKey(string.Format(DictKeyFormat, key, i)))
+                dictKey = LoadBasicType(keyType, string.Format(DictKeyFormat, key, i));
+
+            if (valueType.GetCustomAttribute(typeof(CanSaveNLoad)) != null)
+                dictValue = Load(valueType, string.Empty, string.Format(DictValueFormat, key, i));
+            else if (PlayerPrefs.HasKey(string.Format(DictKeyFormat, key, i)))
+                dictValue = LoadBasicType(valueType, string.Format(DictValueFormat, key, i));
+
+            if (dictKey == null || dictValue == null)
+                return null;
+
+            dictionary.Add(dictKey, dictValue);
+        }
+
+        return dictionary;
     }
 }
 
@@ -196,7 +284,7 @@ public static partial class SaveNLoad
         if (attribute == null)
             return;
 
-        fields = type.GetFields();
+        fields = type.GetFields(attribute.BindingFlags);
         foreach (FieldInfo field in fields)
         {
             string key;
@@ -208,13 +296,15 @@ public static partial class SaveNLoad
             if (field.FieldType.GetCustomAttribute(typeof(CanSaveNLoad)) != null)
                 Delete(field.FieldType, string.Empty, Path.Combine(attribute.PathName, name));
             else if (field.FieldType.IsGenericType && (field.FieldType.GetGenericTypeDefinition() == typeof(List<>)))
-                DeleteListType(field.FieldType, key, ListFormat);
+                DeleteListType(field.FieldType, key);
+            else if (field.FieldType.IsGenericType && (field.FieldType.GetGenericTypeDefinition() == typeof(Dictionary<,>)))
+                DeleteDictionaryType(field.FieldType, key);
             else if (PlayerPrefs.HasKey(key))
                 PlayerPrefs.DeleteKey(key);
         }
     }
 
-    private static void DeleteListType(Type type, string key, string format)
+    private static void DeleteListType(Type type, string key)
     {
         Type elementType = type.GetGenericArguments().Single();
         string countKey = key + ListCount;
@@ -227,9 +317,36 @@ public static partial class SaveNLoad
         for (int i = 0; i < count; i++)
         {
             if (elementType.GetCustomAttribute(typeof(CanSaveNLoad)) != null)
-                Delete(elementType, string.Empty, string.Format(format, key, i));
-            else if (PlayerPrefs.HasKey(string.Format(format, key, i)))
-                PlayerPrefs.DeleteKey(string.Format(format, key, i));
+                Delete(elementType, string.Empty, string.Format(ListFormat, key, i));
+            else if (PlayerPrefs.HasKey(string.Format(ListFormat, key, i)))
+                PlayerPrefs.DeleteKey(string.Format(ListFormat, key, i));
+        }
+
+        PlayerPrefs.DeleteKey(countKey);
+    }
+
+    private static void DeleteDictionaryType(Type type, string key)
+    {
+        Type keyType = type.GetGenericArguments()[0];
+        Type valueType = type.GetGenericArguments()[1];
+        string countKey = key + DictCount;
+        int count;
+
+        if ( !PlayerPrefs.HasKey(countKey) )
+            return;
+
+        count = PlayerPrefs.GetInt(countKey);
+        for (int i = 0; i < count; i++)
+        {
+            if (keyType.GetCustomAttribute(typeof(CanSaveNLoad)) != null)
+                Delete(keyType, string.Empty, string.Format(DictKeyFormat, key, i));
+            else if (PlayerPrefs.HasKey(string.Format(DictKeyFormat, key, i)))
+                PlayerPrefs.DeleteKey(string.Format(DictKeyFormat, key, i));
+
+            if (valueType.GetCustomAttribute(typeof(CanSaveNLoad)) != null)
+                Delete(valueType, string.Empty, string.Format(DictValueFormat, key, i));
+            else if (PlayerPrefs.HasKey(string.Format(DictValueFormat, key, i)))
+                PlayerPrefs.DeleteKey(string.Format(DictValueFormat, key, i));
         }
 
         PlayerPrefs.DeleteKey(countKey);
