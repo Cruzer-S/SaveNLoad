c556396aed11ca5e5713d94cd9088a1887d6b2f5
Cruzer-S <yyyynoom@gmail.com>
2023-06-07 12:20:57 +0900

    remove unused using statement

--- Assets/Scripts/SaveNLoad.cs
+++ Assets/Scripts/SaveNLoad.cs
@@ -1,11 +1,9 @@
 using System;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Collections;
 using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Linq;
-
 
 using UnityEngine;
 
